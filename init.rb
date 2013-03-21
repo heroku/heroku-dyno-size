fc13e0d90349b2db1d7c02aebeb8f57de9e576e0
zeke <zeke@sikelianos.com>
2013-03-21 14:08:10 -0700

    mvp

--- init.rb
+++ init.rb
@@ -21,19 +21,37 @@ class Heroku::Command::Ps
     end
   end
 
+  # ps:size [web=SIZE]
+  #
+  # set the size of your web worker
+  #
   def size
-    case args.first
-    when "1", "1x", "1X" then hputs "1!"
-    when "2", "2x", "2X" then hputs "2!"
-    else
-      raise(Heroku::Command::CommandFailed, "Usage: heroku ps:size [<1X | 2X>]\nMust specify SIZE to resize.")
+    if args.empty? || args.size > 1
+      raise_size_usage
     end
+
+    unless args.first =~ /web=/
+      raise(Heroku::Command::CommandFailed, "\"web\" is currently the only sizeable type.")
+    end
+
+    size = args.first.split("=").last
+    case size
+    when "1", "1x", "1X" then hputs "Resizing web to 1X ($0.05/dyno-hour)... done\nRestarting app... done"
+    when "2", "2x", "2X" then hputs "Resizing web to 2X ($0.10/dyno-hour)... done\nRestarting app... done"
+    else
+      raise(Heroku::Command::CommandFailed, "#{size} is not a valid size.\nValid sizes are 1X and 2X.")
+    end
+
   end
 
 private
 
   def optional_app
     app rescue nil
+  end
+
+  def raise_size_usage
+    raise(Heroku::Command::CommandFailed, "Usage: heroku ps:size web=[1X|2X]")
   end
 
 end
