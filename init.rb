d9746e9627baa1f7906b138625540cf5f0e16f09
Maximilian Schoening <mschoening@me.com>
2013-03-21 18:28:14 -0700

    Implement new proposal

--- init.rb
+++ init.rb
@@ -1,44 +1,62 @@
-# require File.expand_path("../lib/ps", __FILE__)
-
 require "heroku/command"
 require "heroku/command/ps"
 
 class Heroku::Command::Ps
 
-  # ps:size web=SIZE
+  # ps:size PROCESS1=SIZE1 [PROCESS2=SIZE2 ...]
   #
-  # set the size of your web worker
+  # resize processes to the given size
+  #
+  # Examples:
+  #
+  # $ heroku ps:size web=2x worker=1x
+  # Resizing web processes to 2X ($0.10/dyno-hour)... done, now 2X
+  # Resizing worker processes to 1X ($0.05/dyno-hour)... done, now 1X
   #
   def size
     app
+    dyno_price = 0.05
 
-    if args.empty? || args.size > 1
-      styled_array [["web", "2X ($0.10/dyno-hour)"],["worker", "1X ($0.05/dyno-hour)"]]
+    if args.empty?
+      styled_array [
+        ["web:", "2X ($0.10/dyno-hour)"],
+        ["worker:", "1X ($0.05/dyno-hour)"],
+        ["qcworker:", "1X ($0.05/dyno-hour)"],
+        ["clock:", "1X ($0.05/dyno-hour)"],
+      ]
       exit
     end
 
-    unless args.first =~ /web=/
-      raise(Heroku::Command::CommandFailed, "\"web\" is currently the only sizeable type.")
+    if app == "app-by-unconfirmed-owner"
+      raise(Heroku::Command::CommandFailed, "Resizing web to 2X $(0.10/dyno-hour)... failed\nYou need to confirm your account first.")
     end
 
-    if app == "one-dyno-app" && args.first =~ /2/
-      raise(Heroku::Command::CommandFailed, "Resizing to 2X... failed\n2X is only available for paid apps.\nScale app to 2 or more processes\nExample: heroku ps:scale web=2")
+    # heroku ps:size web=2x zookeeper=1x
+    # Resizing web processes to 2X ($0.10/dyno-hour)... done, now 2X
+    # Resizing zookeeper processes... failed
+    #  !    No such type as zookeeper
+
+    # heroku ps:size web=2x fudge
+    # Resizing web processes to 2X ($0.10/dyno-hour)... done, now 2X
+
+    changes = {}
+    args.each do |arg|
+      if arg =~ /^([a-zA-Z0-9_]+)(=\d+)([xX]?)$/
+        changes[$1] = $2
+      end
     end
 
-    if app == "free-app-in-app-tiers-experiment" && args.first =~ /2/
-      raise(Heroku::Command::CommandFailed, "Resizing to 2X... failed\n2X is only available for paid apps.\nUpgrade app to production\nExample: heroku ")
+    if changes.empty?
+      error("Usage: heroku ps:size PROCESS1=SIZE1 [PROCESS2=SIZE2 ...]\nMust specify PROCESS and SIZE to resize.")
     end
 
-    size = args.first.split("=").last
-    case size
-    when "1", "1x", "1X" then hputs "Resizing web to 1X ($0.05/dyno-hour)... done\nRestarting app... done"
-    when "2", "2x", "2X" then hputs "Resizing web to 2X ($0.10/dyno-hour)... done\nRestarting app... done"
-    else
-      raise(Heroku::Command::CommandFailed, "#{size} is not a valid size.\nValid sizes are 1X and 2X.")
+    changes.keys.sort.each do |process|
+      size = changes[process].gsub!("=", "")
+      action("Resizing #{process} processes to #{size}X $(#{sprintf("%.2f", dyno_price * size.to_i)}/dyno-hour)") do
+        status("now running #{size}X")
+      end
     end
 
   end
-
-private
 
 end
